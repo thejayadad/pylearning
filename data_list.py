5bdf5769c44a273f85262035feb23dc949a43f2d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-20 22:26:01 -0400

    update

---
+++ data_list.py
@@ -0,0 +1,12 @@
+#Lists are used to store multiple items in a single variable
+
+grocery_list = ["eggs", "seafood", "lettuce"]
+
+print(grocery_list)
+print(type(grocery_list))
+print(grocery_list[0])
+grocery_list[1] = "potatoes"
+print(grocery_list)
+print(len(grocery_list))
+grocery_list.append("milk")
+print(grocery_list)
