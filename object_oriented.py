6f012fea78c694512240df9be6f9406f0d78b2dd
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-25 18:26:52 -0400

    update

---
+++ object_oriented.py
@@ -0,0 +1,22 @@
+
+
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@kerak.com"
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+
+
+emp_1 = Employee('Jace', 'Dunbar', 50000)
+emp_2 = Employee('Jada', 'Dunbar', 60000)
+
+print(emp_1)
+print(emp_2.email)
+print(emp_2.fullname())
